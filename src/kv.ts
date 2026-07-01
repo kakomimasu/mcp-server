7ca4e1fbf58c2aafa4e7eb27a5e3765d184902c7
kamekyame <kamekyame@outlook.com>
2026-07-01 20:26:08 +0900

    👍 kvのキーを変更

--- src/kv.ts
+++ src/kv.ts
@@ -10,16 +10,16 @@ export type SessionData = {
 export const kv = await Deno.openKv();
 
 export async function getSessionData(sessionId: string) {
-  const data = await kv.get<SessionData>(["sessions", sessionId]);
+  const data = await kv.get<SessionData>(["mcp-sessions", sessionId]);
   return data.value;
 }
 
 export async function setSessionData(sessionId: string, data: SessionData) {
-  await kv.set(["sessions", sessionId], data, {
+  await kv.set(["mcp-sessions", sessionId], data, {
     expireIn: 1000 * 60 * 60 * 24, // DELETE が送られてこないと情報が残ったままなので、約1日でexpireさせる
   });
 }
 
 export async function deleteSessionData(sessionId: string) {
-  await kv.delete(["sessions", sessionId]);
+  await kv.delete(["mcp-sessions", sessionId]);
 }
